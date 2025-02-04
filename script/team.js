3b65eed32e05b067a842b83bc0199ba8fb182aef
Twisuki <Twisuki@outlook.com>
2025-02-04 12:42:09 +0800

    Team页面主照片轮播

--- script/team.js
+++ script/team.js
@@ -9,3 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
 	updateHeaderMargin();
 	window.addEventListener("resize", updateHeaderMargin);
 });
+
+
+// Picture轮播
+document.addEventListener("DOMContentLoaded", function () {
+	const img = document.getElementsByClassName("pic");
+	let index = 0;
+
+	function pictureSwitcher () {
+		img[index].classList.remove("active");
+		index = (index + 1) % img.length;
+		img[index].classList.add("active");
+	}
+
+	setInterval(pictureSwitcher, 5000)
+});
